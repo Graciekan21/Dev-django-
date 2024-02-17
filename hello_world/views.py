c8cbc53b51c1149cb4d5924205d11da848750ac2
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-02-17 22:03:47 +0000

    Added a function to the views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-if request.method == "POST" ("You must have POSTed something")
+if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
     else:
         return HttpResponse(request.method)
- return HttpRequest
 def about_me(request):
     return HttpResponse("This would be the about page")
 def index(request):
